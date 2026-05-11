8a976045c1652efd608b7164a7b854bc6b99bcf4
hanrrysantos <hanrry.jsantos@gmail.com>
2026-05-11 08:24:24 -0300

    feat: add notification entity and enums

---
+++ notification-service/src/main/java/br/com/hanrry/notification_service/database/model/NotificationEntity.java
@@ -0,0 +1,47 @@
+package br.com.hanrry.notification_service.database.model;
+
+import br.com.hanrry.notification_service.enums.NotificationStatus;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Entity
+@Table(name = "tb_notifications")
+public class NotificationEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private UUID orderId;
+
+    private String emailFrom;
+
+    @Column(nullable = false)
+    private String clientEmail;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    @Column(columnDefinition = "TEXT")
+    private String subject;
+
+    @Enumerated(EnumType.STRING)
+    private NotificationStatus status;
+
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate(){
+        this.createdAt = LocalDateTime.now();
+        if(this.status == null) this.status = NotificationStatus.PENDING;
+    }
+}
---
+++ notification-service/src/main/java/br/com/hanrry/notification_service/enums/NotificationStatus.java
@@ -0,0 +1,8 @@
+package br.com.hanrry.notification_service.enums;
+
+public enum NotificationStatus {
+
+    SENT,
+    FAILED,
+    PENDING
+}
