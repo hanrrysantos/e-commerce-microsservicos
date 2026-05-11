e09ef297bd6bf6fe42be2cca66ad584d16706bb2
hanrrysantos <hanrry.jsantos@gmail.com>
2026-05-11 08:21:49 -0300

    feat: add rabbitmq consumer config and deliveries consumer

---
+++ deliveries-service/src/main/java/br/com/hanrry/deliveries_service/consumer/DeliveriesConsumer.java
@@ -0,0 +1,23 @@
+package br.com.hanrry.deliveries_service.consumer;
+
+import br.com.hanrry.deliveries_service.config.RabbitMQConfig;
+import br.com.hanrry.deliveries_service.dto.OrderEventDTO;
+import br.com.hanrry.deliveries_service.service.DeliveriesService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class DeliveriesConsumer {
+
+    private final DeliveriesService deliveriesService;
+
+    @RabbitListener(queues = RabbitMQConfig.QUEUE_DELIVERIES)
+    public void consumeOrderEvent(OrderEventDTO event){
+        log.info(">>> Mensagem recebida na fila de Logística: {}", event.orderCode());
+        deliveriesService.processOrder(event);
+    }
+}
