fb32600d764dfadd3893e56b7b7b5e7d9fbabb6c
hanrrysantos <hanrry.jsantos@gmail.com>
2026-05-11 15:26:17 -0300

    feat: add deliveries service

--- deliveries-service/src/main/java/br/com/hanrry/deliveries_service/service/DeliveriesService.java
+++ deliveries-service/src/main/java/br/com/hanrry/deliveries_service/service/DeliveriesService.java
@@ -2,11 +2,18 @@ package br.com.hanrry.deliveries_service.service;
 
 import br.com.hanrry.deliveries_service.database.model.DeliveriesEntity;
 import br.com.hanrry.deliveries_service.database.repository.IDeliveriesRepository;
-import br.com.hanrry.deliveries_service.dto.OrderEventDTO;
+import br.com.hanrry.deliveries_service.dto.event.OrderEventDTO;
+import br.com.hanrry.deliveries_service.dto.response.DeliveriesResponseDTO;
+import br.com.hanrry.deliveries_service.enums.DeliveriesStatus;
+import br.com.hanrry.deliveries_service.exception.DeliveryNotFoundException;
 import br.com.hanrry.deliveries_service.mapper.IDeliveriesMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -21,7 +28,43 @@ public class DeliveriesService {
         DeliveriesEntity entity = deliveriesMapper.toEntity(event);
 
         deliveriesRepository.save(entity);
+    }
 
+    public DeliveriesResponseDTO findDeliveryById(UUID id){
+        DeliveriesEntity delivery = deliveriesRepository.findById(id).orElseThrow(
+                () -> new DeliveryNotFoundException("Delivery not found with this id: " + id)
+        );
+
+        return deliveriesMapper.toDTO(delivery);
+    }
+
+    public List<DeliveriesResponseDTO> findAllDeliveries(){
+        List<DeliveriesEntity> deliveries = deliveriesRepository.findAll();
+
+        return deliveriesMapper.toDTOList(deliveries);
+    }
+
+    public DeliveriesResponseDTO findDeliveryByTrackingCode(String trackingCode){
+        DeliveriesEntity delivery = deliveriesRepository.findByTrackingCode(trackingCode)
+                .orElseThrow(() -> new DeliveryNotFoundException("Delivery not found with this tracking code: " + trackingCode));
+
+        return deliveriesMapper.toDTO(delivery);
+    }
+
+    public DeliveriesResponseDTO updateDeliveryStatus(UUID id, DeliveriesStatus status){
+        DeliveriesEntity delivery = deliveriesRepository.findById(id).orElseThrow(
+                () -> new DeliveryNotFoundException("Delivery not found with this id: " + id)
+        );
+
+        if (status == DeliveriesStatus.DELIVERED) {
+            delivery.setDeliveredAt(LocalDateTime.now());
+        }
+
+        delivery.setStatus(status);
+
+        DeliveriesEntity savedDelivery = deliveriesRepository.save(delivery);
+
+        return deliveriesMapper.toDTO(savedDelivery);
     }
     
 }
