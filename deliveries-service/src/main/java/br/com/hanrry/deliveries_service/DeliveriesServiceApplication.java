f9db6956af87fcabe42f8948b59750f1772fbd33
hanrrysantos <hanrry.jsantos@gmail.com>
2026-05-11 08:22:54 -0300

    feat: add deliveries application

---
+++ deliveries-service/src/main/java/br/com/hanrry/deliveries_service/DeliveriesServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.hanrry.deliveries_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveriesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveriesServiceApplication.class, args);
+	}
+
+}
