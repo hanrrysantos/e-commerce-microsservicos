4393dc855cc1d1afc1278b12ad0963adbda47154
hanrrysantos <hanrry.jsantos@gmail.com>
2026-05-11 13:39:09 -0300

    feat: add event in deliveries

--- deliveries-service/src/main/java/br/com/hanrry/deliveries_service/dto/OrderEventDTO.java
+++ deliveries-service/src/main/java/br/com/hanrry/deliveries_service/dto/OrderEventDTO.java
@@ -1,6 +1,9 @@
 package br.com.hanrry.deliveries_service.dto;
 
+import br.com.hanrry.deliveries_service.enums.OrderStatus;
+
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.UUID;
 
 public record OrderEventDTO(
@@ -8,6 +11,9 @@ public record OrderEventDTO(
         String orderCode,
         String clientName,
         String clientEmail,
-        BigDecimal totalValue
+        OrderStatus status,
+        BigDecimal totalValue,
+        List<OrderItemEventDTO> items,
+        OrderAddressEventDTO address
 ){
 }
