c4e4ebafa756bd646894bb03ae619c791df5ecbc
hanrrysantos <hanrry.jsantos@gmail.com>
2026-05-11 15:28:02 -0300

    feat: add deliveries mapper and repository

--- deliveries-service/src/main/java/br/com/hanrry/deliveries_service/database/repository/IDeliveriesRepository.java
+++ deliveries-service/src/main/java/br/com/hanrry/deliveries_service/database/repository/IDeliveriesRepository.java
@@ -3,7 +3,10 @@ package br.com.hanrry.deliveries_service.database.repository;
 import br.com.hanrry.deliveries_service.database.model.DeliveriesEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface IDeliveriesRepository extends JpaRepository<DeliveriesEntity, UUID> {
+
+    Optional<DeliveriesEntity> findByTrackingCode(String trackingCode);
 }
--- deliveries-service/src/main/java/br/com/hanrry/deliveries_service/mapper/IDeliveriesMapper.java
+++ deliveries-service/src/main/java/br/com/hanrry/deliveries_service/mapper/IDeliveriesMapper.java
@@ -1,21 +1,26 @@
 package br.com.hanrry.deliveries_service.mapper;
 
 import br.com.hanrry.deliveries_service.database.model.DeliveriesEntity;
-import br.com.hanrry.deliveries_service.dto.OrderEventDTO;
+import br.com.hanrry.deliveries_service.dto.event.OrderEventDTO;
+import br.com.hanrry.deliveries_service.dto.response.DeliveriesResponseDTO;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface IDeliveriesMapper {
 
-
     @Mapping(target = "id", ignore = true)
-    @Mapping(target = "orderId", source = "id")
-    @Mapping(target = "status", constant = "PENDING")
-    @Mapping(target = "productType", source = "typeProduct")
-    @Mapping(target = "createdAt", ignore = true)
-    @Mapping(target = "updatedAt", ignore = true)
-    @Mapping(target = "deliveredAt", ignore = true)
-    @Mapping(target = "trackingCode", ignore = true)
+    @Mapping(source = "id", target = "orderId")
+    @Mapping(source = "address.city", target = "city")
+    @Mapping(source = "address.street", target = "street")
+    @Mapping(source = "address.state", target = "state")
+    @Mapping(source = "address.zipCode", target = "zipCode")
+    @Mapping(source = "address.country", target = "country")
     DeliveriesEntity toEntity(OrderEventDTO eventDTO);
+
+    DeliveriesResponseDTO toDTO(DeliveriesEntity delivery);
+
+    List<DeliveriesResponseDTO> toDTOList(List<DeliveriesEntity> deliveries);
 }
