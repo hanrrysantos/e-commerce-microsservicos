2834622e447c3a6c920edd65d1ceaf397abdacfc
hanrrysantos <hanrry.jsantos@gmailcom>
2026-05-12 20:17:10 -0300

    feat: add notification mapper and repository

--- notification-service/src/main/java/br/com/hanrry/notification_service/database/repository/INotificationRepository.java
+++ notification-service/src/main/java/br/com/hanrry/notification_service/database/repository/INotificationRepository.java
@@ -3,7 +3,10 @@ package br.com.hanrry.notification_service.database.repository;
 import br.com.hanrry.notification_service.database.model.NotificationEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface INotificationRepository extends JpaRepository<NotificationEntity, UUID> {
+
+    List<NotificationEntity> findByClientEmail(String email);
 }
--- notification-service/src/main/java/br/com/hanrry/notification_service/mapper/INotificationMapper.java
+++ notification-service/src/main/java/br/com/hanrry/notification_service/mapper/INotificationMapper.java
@@ -1,9 +1,13 @@
 package br.com.hanrry.notification_service.mapper;
 
 import br.com.hanrry.notification_service.database.model.NotificationEntity;
-import br.com.hanrry.notification_service.dto.OrderEventDTO;
+import br.com.hanrry.notification_service.dto.event.OrderEventDTO;
+import br.com.hanrry.notification_service.dto.event.UserEventDTO;
+import br.com.hanrry.notification_service.dto.response.NotificationResponseDTO;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface INotificationMapper {
@@ -12,5 +16,20 @@ public interface INotificationMapper {
     @Mapping(target = "createdAt", ignore = true)
     @Mapping(target = "status", ignore = true)
     @Mapping(target = "orderId", source = "id")
-    NotificationEntity toEntity(OrderEventDTO event);
+    @Mapping(target = "name", source = "clientName")
+    NotificationEntity OrderEventToEntity(OrderEventDTO event);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "status", ignore = true)
+    @Mapping(target = "orderId", ignore = true)
+    @Mapping(target = "clientEmail", source = "email")
+    NotificationEntity UserEventToEntity(UserEventDTO event);
+
+
+    NotificationResponseDTO toDTO(NotificationEntity entity);
+
+
+    List<NotificationResponseDTO> toDTOList(List<NotificationEntity> notifications);
+
 }
