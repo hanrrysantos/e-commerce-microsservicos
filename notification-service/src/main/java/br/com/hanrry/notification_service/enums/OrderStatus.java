4edab6b7dfef5542318456779bcd5b7db9d283b2
hanrrysantos <hanrry.jsantos@gmailcom>
2026-05-14 15:04:07 -0300

    feat(notification): handle payment notification events

---
+++ notification-service/src/main/java/br/com/hanrry/notification_service/enums/OrderStatus.java
@@ -0,0 +1,9 @@
+package br.com.hanrry.notification_service.enums;
+
+public enum OrderStatus {
+    PENDING,
+    CONFIRMED,
+    CANCELLED,
+    DELIVERED,
+    IN_TRANSIT
+}
---
+++ notification-service/src/main/java/br/com/hanrry/notification_service/enums/PaymentMethod.java
@@ -0,0 +1,7 @@
+package br.com.hanrry.notification_service.enums;
+
+public enum PaymentMethod {
+    PIX,
+    CREDIT_CARD,
+    BOLETO
+}
