2e1ca7a684b2fb710e87cf653959c207235d74b7
hanrrysantos <hanrry.jsantos@gmailcom>
2026-05-20 12:08:27 -0300

    feat: add product entity

--- product-service/src/main/java/br/com/hanrry/product_service/database/model/ProductEntity.java
+++ product-service/src/main/java/br/com/hanrry/product_service/database/model/ProductEntity.java
@@ -1,4 +1,56 @@
 package br.com.hanrry.product_service.database.model;
 
+import br.com.hanrry.product_service.enums.Category;
+import br.com.hanrry.product_service.enums.ProductStatus;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Entity
+@Table(name = "tb_products")
 public class ProductEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String description;
+
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal price;
+
+    @Column(nullable = false)
+    private Integer stockQuantity;
+
+    @Enumerated(EnumType.STRING)
+    private Category category;
+
+    @Enumerated(EnumType.STRING)
+    private ProductStatus productStatus;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate(){
+        this.createdAt = LocalDateTime.now();
+        this.productStatus = ProductStatus.ACTIVE;
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
 }
