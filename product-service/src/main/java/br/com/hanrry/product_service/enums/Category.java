3e7c3d9f3367927e9193f552f0ec6ce439cbcf11
hanrrysantos <hanrry.jsantos@gmailcom>
2026-05-20 12:09:45 -0300

    feat: add category enum

---
+++ product-service/src/main/java/br/com/hanrry/product_service/enums/Category.java
@@ -0,0 +1,16 @@
+package br.com.hanrry.product_service.enums;
+
+public enum Category {
+
+    ELECTRONICS,
+    CLOTHING,
+    HOME,
+    SPORTS,
+    BOOKS,
+    BEAUTY,
+    FOOD,
+    TOYS,
+    AUTOMOTIVE,
+    HEALTH
+
+}
