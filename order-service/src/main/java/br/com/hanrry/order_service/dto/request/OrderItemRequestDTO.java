14d4f20674b9dee401d320130f54aac8779d2cdc
hanrrysantos <hanrry.jsantos@gmailcom>
2026-05-20 21:00:01 -0300

    feat: removing some attributes

--- order-service/src/main/java/br/com/hanrry/order_service/dto/request/OrderItemRequestDTO.java
+++ order-service/src/main/java/br/com/hanrry/order_service/dto/request/OrderItemRequestDTO.java
@@ -1,12 +1,9 @@
 package br.com.hanrry.order_service.dto.request;
 
-import java.math.BigDecimal;
 import java.util.UUID;
 
 public record OrderItemRequestDTO(
     UUID productId,
-    String productName,
-    Integer quantity,
-    BigDecimal unitPrice
+    Integer quantity
 ) {
 }
