f926ba974f7677a1ef76ecc056ed6be378b8f82a
hanrrysantos <hanrry.jsantos@gmailcom>
2026-05-27 10:30:47 -0300

    feat: add admin demo

---
+++ user-service/src/main/resources/db/migration/V2__insert_demo_admin.sql
@@ -0,0 +1,14 @@
+INSERT INTO tb_users
+(id, name, email, password, status, role, created_at, updated_at)
+VALUES
+    (
+        gen_random_uuid(),
+        'Admin Demo',
+        'admin@demo.com',
+        '$2a$12$wqV1V7QpxCe0HsTOL4qfSuCHj1Jw8h55W93NccD88fRoGeYnXdq/m',
+        'REGISTERED',
+        'ADMIN',
+        CURRENT_TIMESTAMP,
+        CURRENT_TIMESTAMP
+    );
+ON CONFLICT (email) DO NOTHING;
