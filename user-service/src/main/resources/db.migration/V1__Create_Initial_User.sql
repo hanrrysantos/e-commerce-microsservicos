559b79626c1e2e6bed467922304576566ea77783
hanrrysantos <hanrry.jsantos@gmailcom>
2026-05-27 09:38:00 -0300

    feat: create migrations for products and user demo

---
+++ user-service/src/main/resources/db.migration/V1__Create_Initial_User.sql
@@ -0,0 +1,12 @@
+INSERT INTO tb_users
+(name, email, password, status, role, created_at, updated_at)
+VALUES
+    (
+        'Cliente Demo',
+        'cliente@demo.com',
+        '$2a$12$wqV1V7QpxCe0HsTOL4qfSuCHj1Jw8h55W93NccD88fRoGeYnXdq/m',
+        'REGISTERED',
+        'CLIENT',
+        CURRENT_TIMESTAMP,
+        CURRENT_TIMESTAMP
+    );
