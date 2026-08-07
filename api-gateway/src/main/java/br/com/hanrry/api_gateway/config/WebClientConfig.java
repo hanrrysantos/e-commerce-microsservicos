44c07728ac8b2cf675ee18601b64de46893c8a1d
hanrrysantos <hanrry.jsantos@gmailcom>
2026-08-06 23:35:25 -0300

    configura WebClient com timeouts adequados ao cold start

---
+++ api-gateway/src/main/java/br/com/hanrry/api_gateway/config/WebClientConfig.java
@@ -0,0 +1,24 @@
+package br.com.hanrry.api_gateway.config;
+
+import io.netty.channel.ChannelOption;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+
+import java.time.Duration;
+
+@Configuration
+public class WebClientConfig {
+
+    @Bean
+    public WebClient.Builder webClientBuilder() {
+        HttpClient httpClient = HttpClient.create()
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 30_000)
+                .responseTimeout(Duration.ofSeconds(60));
+
+        return WebClient.builder()
+                .clientConnector(new ReactorClientHttpConnector(httpClient));
+    }
+}
